603c508f5095b7b3d22088139c817763c9355815
razmag <46993372+RazMag@users.noreply.github.com>
2023-11-17 11:31:01 +0000

    added docstrings

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 
+import os
+import re
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
 from requests_oauthlib import OAuth1Session
 from dotenv import load_dotenv
-import os
-import re
+
 
 
 load_dotenv()
 login_manager = LoginManager()
-
 app = Flask(__name__)
 login_manager.init_app(app)
 app.secret_key = os.environ.get("APP_SECRET")
@@ -19,8 +19,33 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 db = SQLAlchemy(app)
 
 class User(db.Model, UserMixin):
+    """
+    Represents a user in the system.
+
+    Attributes:
+        id (str): The unique identifier for the user.
+        password (str): The hashed password for the user.
+    """
     id = db.Column(db.String(80), primary_key=True)
     password = db.Column(db.String(120))
+
+class Tweet(db.Model):
+    """
+    Represents a tweet object in the database.
+
+    Attributes:
+    id (int): The unique identifier for the tweet.
+    user_id (str): The unique identifier for the user who posted the tweet.
+    datetime (datetime): The date and time the tweet should be posted.
+    data (str): The content of the tweet.
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(80), db.ForeignKey('user.id'))
+    datetime = db.Column(db.DateTime, nullable=False)
+    data = db.Column(db.String(280), nullable=False)
+    media = db.Column(db.String(1200), nullable=True)
+    super_follows = db.Column(db.Boolean, nullable=False)
+    posted = db.Column(db.Boolean, nullable=False)
 
 with app.app_context():
     db.create_all()
@@ -28,20 +53,51 @@ with app.app_context():
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(user_id)
+    """
+    Load a user from the session by their ID.
+
+    Args:
+        user_id (int): The ID of the user to load.
+
+    Returns:
+        User: The user object corresponding to the given ID, or None if no such user exists.
+    """
+    return User.session.get(user_id)
 
 @app.route('/protected')
 @login_required
 def protected():
+    """
+    Returns a JSON response indicating that the route is protected.
+    """
     return jsonify(message='This is a protected route')
 
 @app.route('/reset_db', methods=['POST'])
 def reset_db():
+    """
+    Resets the database by dropping all tables and recreating them.
+    
+    Returns:
+        A JSON response with a success message and a 200 status code.
+    """
     db.drop_all()
     db.create_all()
     return jsonify({"message": "Database reset successfully"}), 200
 
 def validate_password(password):
+    """
+    Validates a password based on the following criteria:
+    - Must be at least 8 characters long
+    - Must contain at least one lowercase letter
+    - Must contain at least one uppercase letter
+    - Must contain at least one digit
+    
+    Args:
+    password (str): The password to be validated
+    
+    Returns:
+    bool: True if the password is valid, False otherwise
+    """
     if len(password) < 8:
         return False
     if not re.search("[a-z]", password):
@@ -54,6 +110,12 @@ def validate_password(password):
 
 @app.route('/signup', methods=['POST'])
 def signup():
+    """
+    Registers a new user with the provided username and password.
+
+    Returns:
+        A JSON response containing a success or error message.
+    """
     data = request.get_json()
     if not data:
         return jsonify({"error": "Missing JSON data"}), 400
@@ -72,6 +134,13 @@ def signup():
 
 @app.route('/login', methods=['POST'])
 def login():
+    """
+    Logs in a user with the provided username and password.
+
+    Returns:
+        A JSON response containing either an error message and a 401 status code,
+        or a success message and a 200 status code.
+    """
     data = request.get_json()
     username = data.get('username')
     password = data.get('password')
@@ -84,9 +153,21 @@ def login():
 @app.route('/logout')
 @login_required
 def logout():
+    """
+    Logs out the current user.
+    
+    Returns:
+        A tuple containing a JSON response with a success message and a 200 status code.
+    """
     logout_user()
     return jsonify({"message": "Logged out successfully"}), 200
 
+@app.route('/health_check')
+def health_check():
+    """
+    Returns a JSON response indicating that the application is healthy.
+    """
+    return jsonify({"message": "Healthy"}), 200
 
 @app.route('/post')
 def post():
@@ -151,9 +232,7 @@ def post():
     return response.json()
     
 
-@app.route('/health_check')
-def health_check():
-    return jsonify({"message": "Healthy"}), 200
+
 
 
 if __name__ == '__main__':
